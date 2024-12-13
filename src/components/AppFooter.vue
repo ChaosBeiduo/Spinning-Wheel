df21ad59dfe015579730141a7b6f091949cf32b6
ChaosBeiduo <chaosbeiduo@outlook.com>
2024-12-13 10:21:48 +0800

    feat: Add footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <p>
+      <a href="https://github.com/ChaosBeiduo/Spinning-Wheel">Code</a> | &copy;
+      2024 <a href="https://chaosbeiduo.github.io/">ChaosBeiduo</a>. All rights
+      reserved.
+    </p>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+footer {
+  padding: 20px;
+
+  color: #aaa;
+  font-size: 13px;
+  text-align: center;
+}
+
+a {
+  color: #aaa;
+}
+</style>
